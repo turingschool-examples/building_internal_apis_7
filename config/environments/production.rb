789e0083b02ef2ae0c49d46e04bb5e6136fb4021
Michael Dao <mike@turing.edu>
2023-06-13 11:17:30 -0600

    Updates to Ruby 3.2.2 and rails 7.0.5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "building_internal_apis_7_production"
+  # config.active_job.queue_name_prefix = "building_internal_apis7_production"
 
   config.action_mailer.perform_caching = false
 
